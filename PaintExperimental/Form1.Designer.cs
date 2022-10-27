cb79aafe39dd0ee6bb92d153eb27199b9e809c45
a.repina <annetrepina@mail.ru>
2022-10-27 19:50:02 +0300

    Работаю над рисованием линии.

--- PaintExperimental/Form1.Designer.cs
+++ PaintExperimental/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace PaintExperimental
 {
-    partial class Form1
+    partial class PaintForm
     {
         /// <summary>
         ///  Required designer variable.
@@ -28,20 +28,74 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(PaintForm));
+            this.button1 = new System.Windows.Forms.Button();
+            this.DrawPanel = new System.Windows.Forms.Panel();
+            this.ToolsPanel = new System.Windows.Forms.Panel();
+            this.DrawLineButton = new System.Windows.Forms.Button();
+            this.ToolsPanel.SuspendLayout();
             this.SuspendLayout();
             // 
-            // Form1
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(1088, 481);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Сохранитьъ";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.OnSaveButtonClick);
+            // 
+            // DrawPanel
+            // 
+            this.DrawPanel.Location = new System.Drawing.Point(12, 85);
+            this.DrawPanel.Name = "DrawPanel";
+            this.DrawPanel.Size = new System.Drawing.Size(973, 553);
+            this.DrawPanel.TabIndex = 1;
+            this.DrawPanel.MouseDown += new System.Windows.Forms.MouseEventHandler(this.OnDrawPaneMouseDown);
+            this.DrawPanel.MouseUp += new System.Windows.Forms.MouseEventHandler(this.OnDrawPanelMouseUp);
+            // 
+            // ToolsPanel
+            // 
+            this.ToolsPanel.Controls.Add(this.DrawLineButton);
+            this.ToolsPanel.Location = new System.Drawing.Point(12, 12);
+            this.ToolsPanel.Name = "ToolsPanel";
+            this.ToolsPanel.Size = new System.Drawing.Size(1225, 57);
+            this.ToolsPanel.TabIndex = 2;
+            // 
+            // DrawLineButton
+            // 
+            this.DrawLineButton.Image = ((System.Drawing.Image)(resources.GetObject("DrawLineButton.Image")));
+            this.DrawLineButton.Location = new System.Drawing.Point(238, 3);
+            this.DrawLineButton.Name = "DrawLineButton";
+            this.DrawLineButton.Size = new System.Drawing.Size(24, 23);
+            this.DrawLineButton.TabIndex = 3;
+            this.DrawLineButton.UseVisualStyleBackColor = true;
+            this.DrawLineButton.Click += new System.EventHandler(this.OnDrawLineButtonClick);
+            // 
+            // PaintForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Name = "Form1";
-            this.Text = "Form1";
-            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.ClientSize = new System.Drawing.Size(1384, 1061);
+            this.Controls.Add(this.ToolsPanel);
+            this.Controls.Add(this.DrawPanel);
+            this.Controls.Add(this.button1);
+            this.Name = "PaintForm";
+            this.Text = "Paint 2.0";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.OnFormPaint);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.OnPaintFormMouseUp);
+            this.ToolsPanel.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private Button button1;
+        private Panel DrawPanel;
+        private Panel ToolsPanel;
+        private Button DrawLineButton;
     }
 }
